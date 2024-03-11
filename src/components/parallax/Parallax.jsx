f010e829f9abd2115e021c66e9ac1798569cba30
Shubhangi Naik <shubhanginaik@Shubhangis-MacBook-Air.local>
2024-03-11 17:54:18 +0200

    added animation to Parallax

--- src/components/parallax/Parallax.jsx
+++ src/components/parallax/Parallax.jsx
@@ -1,10 +1,21 @@
+import { useRef } from "react";
 import "./parallax.scss";
-import { motion } from "framer-motion";
+import { motion, useScroll, useTransform } from "framer-motion";
 
-const parallax = ({ type }) => {
+const Parallax = ({ type }) => {
+  const ref = useRef();
+
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start start", "end start"],
+  });
+  const yText = useTransform(scrollYProgress, [0, 1], ["0%", "500%"]);
+  const yBg = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
+
   return (
     <div
       className="parallax"
+      ref={ref}
       style={{
         background:
           type === "services"
@@ -12,14 +23,25 @@ const parallax = ({ type }) => {
             : "linear-gradient(180deg, #111132, #505064)",
       }}
     >
-      <motion.h1>
+      <motion.h1 style={{ y: yText }}>
         {type === "services" ? "What We Do?" : "What We Did?"}
       </motion.h1>
       <motion.div className="mountains"></motion.div>
-      <motion.div className="planets"></motion.div>
-      <motion.div className="stars"></motion.div>
+      <motion.div
+        className="planets"
+        style={{ y: yBg }}
+        animate={{
+          rotate: 180, // Rotate the div 360 degrees
+        }}
+        transition={{
+          duration: 160, // Duration of the animation in seconds
+          loop: Infinity, // Loop the animation infinitely
+          ease: "linear", // Linear easing for smooth rotation
+        }}
+      ></motion.div>
+      <motion.div className="stars" style={{ x: yBg }}></motion.div>
     </div>
   );
 };
 
-export default parallax;
+export default Parallax;
